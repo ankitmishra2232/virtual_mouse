f599f15e32d4dcf327ab9118d1ed3e655c658428
Ankit Mishra <ankitmishra.in.com@gmail.com>
2023-01-30 13:39:56 +0530

    Virtual mouse complete using OPENCV and MEDIAPIPE

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -3,17 +3,22 @@ import numpy as np
 import HandTrackingModule as htm
 import time
 import autopy
+import datetime
 ############################
 wCam,hCam =640,480
+frameR= 100  #frame reduction
+smoothening = 7
 ############################
+pTime =0
+plocx,plocy=0,0
+clocx,clocy=0,0
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-pTime =0
+cap.set(3,wCam)
+cap.set(4,hCam)
 detector = htm.handDetector(maxHands=1)
-
-
+wScr, hScr = autopy.screen.size()
+# print(wScr,hScr)
 while True:
     #1. find hand Landmarks
 
@@ -22,14 +27,44 @@ while True:
     lmList, bbox = detector.findPosition(img)
 
     #2. Get the tip of the index and middle fingers
-    #3. Check which fingers are up
-    #4. Only Index Finger : moving mode
-    #5. Convert Coordinates
-    #6. Smoothen Value
-    #7. Move Mouse
-    #8. Both Index and middle fingres are up: Clicking mode
-    #9. Find Distance between fingures
-    #10. Click mouse if distance short
+
+    if len(lmList)!=0:
+        x1,y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        # print(x1,y1,x2,y2)
+        #3. Check which fingers are up
+        fingers = detector.fingersUp()
+        # print(fingers)
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+        #4. Only Index Finger : moving mode
+        if fingers[1]==1 and fingers[2] ==0:
+            #5. Convert Coordinates
+            x3 = np.interp(x1,(frameR,wCam-frameR),(0,wScr))
+            y3 = np.interp(y1,(frameR,hCam-frameR),(0,hScr))
+
+            #6. Smoothen Value
+            clocx = plocx+(x3-plocx) /smoothening
+            clocy = plocy + (y3 - plocy) / smoothening
+
+            #7. Move Mouse
+            autopy.mouse.move(wScr-clocx,clocy)
+            cv2.circle(img,(x1,y1),15,(255,255,255),cv2.FILLED)
+            plocx,plocy = clocx,clocy
+        #8. Both Index and middle fingres are up: Clicking mode
+        if fingers[1] ==1 and fingers[2]==1:
+            # 9. Find Distance between fingures
+            length, img,lineInfo =detector.findDistance(8,12,img)
+            # print(length)
+            # 10. Click mouse if distance short
+
+            if length<30:
+                cv2.circle(img,(lineInfo[4],lineInfo[5]),15,(0,255,0),cv2.FILLED)
+                
+                autopy.mouse.click()
+
+
+
 
     #11. Frame Rate
     cTime =time.time()
